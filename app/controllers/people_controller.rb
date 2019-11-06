41c869c6cb9b38d7f408b8c77466f42eaddc43e1
Trish Ogen <patriciaogen@gmail.com>
2019-11-06 00:35:59 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,19 +4,29 @@ class PeopleController < ApplicationController
     @person.addresses.build(address_type: 'work')
     @person.addresses.build(address_type: 'home')
   end
- 
+
   def create
     person = Person.create(person_params)
     redirect_to people_path
   end
- 
+
   def index
     @people = Person.all
   end
- 
+
   private
- 
+
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
   end
 end
